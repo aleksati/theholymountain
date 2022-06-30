8b4ac981b8e9f5a337778027446cfd25c7bf5529
AleksanderTidemann <aleksandertid@gmail.com>
2022-06-30 22:46:43 +0200

    change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import cat1 from "../public/img/cat3.gif";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen space-y-8 lg-flex-row">
-      <h1 className="text-4xl text-white">Under utvilking ..</h1>
+      <h1 className="text-4xl text-white">under utvilking</h1>
       <div className="relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-0">
         <Image
           objectFit="cover"
