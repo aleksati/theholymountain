e2b561abf201fa5fcb8f3619906b99fe852ee34a
AleksanderTidemann <aleksandertid@gmail.com>
2022-07-17 20:12:41 +0200

    Deploy

--- pages/api/video/index.js
+++ pages/api/video/index.js
@@ -4,6 +4,6 @@ export default function handler(req, res) {
   // sort the data in decending order according to year
   //   const sortedVideoData = videoData.sort(
   //     (a, b) => Number(b.year) - Number(a.year)
-  //   );
+  //   )
   res.status(200).json(videoData);
 }
