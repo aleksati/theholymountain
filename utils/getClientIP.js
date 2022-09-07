30bb3dad8f90afc1edc6055cd5a644a5b5cba726
AleksanderTidemann <aleksandertid@gmail.com>
2022-09-07 16:27:12 +0200

    Try to split IP address by comma to reduce the number

--- utils/getClientIP.js
+++ utils/getClientIP.js
@@ -1,9 +1,11 @@
-const getClientIP = (req) => {
+const getClientIP = req => {
   const forwarded = req.headers["x-forwarded-for"];
+
   const userIP =
     typeof forwarded === "string"
-      ? forwarded.split(/, /)[0]
+      ? forwarded.split(",")[0]
       : req.socket.remoteAddress;
+
   return userIP;
 };
 
