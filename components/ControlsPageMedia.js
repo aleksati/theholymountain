a7bc86cb06bf130b8b08ba7e945596ce9dba569e
Aleksander Tidemann <aleksandertid@gmail.com>
2022-10-30 10:25:46 +0100

    Update ControlsPageMedia.js

--- components/ControlsPageMedia.js
+++ components/ControlsPageMedia.js
@@ -1,24 +1,33 @@
-import ButtonIconAndText from "./ButtonIconAndText";
-import getCurrTheme from "../functions/getCurrTheme";
+// import ButtonIconAndText from "./ButtonIconAndText";
+// import getCurrTheme from "../functions/getCurrTheme";
 
 const ControlsPageMedia = ({ onClick, filters, activeFilters }) => {
-  const { currTheme } = getCurrTheme();
-  const color =
-    currTheme === "dark"
-      ? "!bg-button-filter-light"
-      : "!bg-button-filter-light";
+  // const { currTheme } = getCurrTheme();
+  // const color =
+  //   currTheme === "dark"
+  //     ? "!bg-button-filter-light"
+  //     : "!bg-button-filter-light";
 
   return (
     <div className="flex items-center justify-center p-4 space-x-4">
-      {filters.map(filter => (
-        <ButtonIconAndText
-          key={filter}
-          keepTextOnSmallScreen={true}
-          text={filter}
-          className={activeFilters.includes(filter) ? color : ""}
-          onClick={() => onClick(filter)}
-        />
-      ))}
+      {filters.map((filter) => {
+        let underline = activeFilters.includes(filter) ? "underline" : "";
+        return (
+          <a
+            onClick={() => onClick(filter)}
+            className={"hover:underline cursor-pointer " + underline}
+          >
+            {filter}
+          </a>
+        );
+        // <ButtonIconAndText
+        //   key={filter}
+        //   keepTextOnSmallScreen={true}
+        //   text={filter}
+        //   className={activeFilters.includes(filter) ? color : ""}
+        //   onClick={() => onClick(filter)}
+        // />
+      })}
     </div>
   );
 };
