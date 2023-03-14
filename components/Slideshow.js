36fd38ad2c611a4648e1a0dfc28d61ed0b2ff912
Aleksander Tidemann <aleksandertid@gmail.com>
2023-03-14 15:08:51 +0100

    more UI updates

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -18,12 +18,12 @@ const properties = {
   defaultIndex: 1,
   nextArrow: (
     <button className="text-primary-dark" aria-label="next slideshow image">
-      <Icon id="nextArrow" iconSize={"text-4xl md:text-4xl"} />
+      <Icon id="nextArrow" iconSize={"text-2xl"} />
     </button>
   ),
   prevArrow: (
     <button className="text-primary-dark" aria-label="previous slideshow image">
-      <Icon id="prevArrow" iconSize={"text-4xl md:text-4xl"} />
+      <Icon id="prevArrow" iconSize={"text-2xl"} />
     </button>
   ),
 };
