eb6d2f3dda14e88e14d304068b69b6b910df8f38
Aleksander Tidemann <aleksandertid@gmail.com>
2023-04-08 18:32:50 +0200

    One small addon..
    Tooltip color

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -7,9 +7,11 @@ export const Tooltip = ({ message, children }) => {
     >
       {children}
       <div
-        className={`absolute z-20 -mb-9 !bg-primary-light !dark:bg-primary-dark bottom-0 flex flex-col text-sm text-center invisible -ml-1 opacity-0 group-hover:animate-tooltip_show`}
+        className={`absolute z-20 -mb-9 bottom-0 flex flex-col text-sm text-center invisible -ml-1 opacity-0 group-hover:animate-tooltip_show`}
       >
-        <span className="relative p-2 leading-none opacity-100">{message}</span>
+        <span className="relative p-2 bg-primary-light dark:bg-primary-dark leading-none opacity-100">
+          {message}
+        </span>
       </div>
     </div>
   );
