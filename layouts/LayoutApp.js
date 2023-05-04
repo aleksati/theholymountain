2fda8dd935081a9562282a34b04ae5ebff450e9e
Aleksander Tidemann <aleksandertid@gmail.com>
2023-05-04 13:59:52 +0200

    Google analytics integration
    First try..

--- layouts/LayoutApp.js
+++ layouts/LayoutApp.js
@@ -1,8 +1,28 @@
 import Footer from "../templates/Footer";
+import Script from "next/script";
 
 const LayoutApp = ({ children }) => {
   return (
     <>
+      {/* Global Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <main className="flex-grow text-size-regular text-primary-light dark:text-primary-dark bg-primary-light dark:bg-primary-dark">
         {children}
       </main>
