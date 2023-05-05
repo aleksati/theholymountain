069ff70c8cec8403e3b855945311c1b60211b57c
Aleksander Tidemann <aleksandertid@gmail.com>
2023-05-05 09:29:38 +0200

    updates

--- layouts/LayoutApp.js
+++ layouts/LayoutApp.js
@@ -1,8 +1,10 @@
 import Footer from "../templates/Footer";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 const LayoutApp = ({ children }) => {
   return (
     <>
+      <GoogleAnalytics />
       <main className="flex-grow text-size-regular text-primary-light dark:text-primary-dark bg-primary-light dark:bg-primary-dark">
         {children}
       </main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import GoogleAnalytics from "../components/GoogleAnalytics";
 import { ThemeProvider } from "next-themes";
 import LayoutApp from "../layouts/LayoutApp";
 import "../styles/globals.css";
@@ -6,7 +5,6 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
-      <GoogleAnalytics />
       <LayoutApp>
         <Component {...pageProps} />
       </LayoutApp>
