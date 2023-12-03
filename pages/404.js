8032db2fc1ece9ff4c2d24e19c73ab8c58cc2a73
Aleksander Tidemann <aleksandertid@gmail.com>
2023-12-03 16:14:01 +0100

    updates

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,11 @@
-import TextOverImageFullscreen from "../components/TextOverImageFullscreen";
-import ImageFullscreen from "../components/ImageFullscreen";
+// import TextOverImageFullscreen from "../components/TextOverImageFullscreen";
+// import ImageFullscreen from "../components/ImageFullscreen";
 import { SITE_DOMAIN } from "../config";
 import Meta from "../components/Meta";
 import Link from "next/link";
 
-const src = "/img/404.png";
-const placeholder = "/img/placeholders/404.png";
+// const src = "/img/404.png";
+// const placeholder = "/img/placeholders/404.png";
 
 const FourOhFour = () => (
   <>
@@ -14,20 +14,16 @@ const FourOhFour = () => (
       description="Oops! It looks like this page is lost in space somewhere!"
       url={`${SITE_DOMAIN}/404`}
     />
-    <div className="relative min-h-screen" id="404">
-      <ImageFullscreen
-        imgSrc={src}
-        placeholder={placeholder}
-        credit="Katinka Hustad"
-        creditLink="https://www.katinkahustad.com/"
-      />
-      <TextOverImageFullscreen>
+    <div className="flex items-center justify-center min-h-screen" id="404">
+      <p>Sorry, this page doesn&apos;t exist.</p>
+      {/* <ImageFullscreen imgSrc={src} placeholder={placeholder} /> */}
+      {/* <TextOverImageFullscreen>
         <Link href="/" passHref>
           <h1 className="font-bold text-primary-dark hover:underline hover:cursor-pointer text-9xl">
             Lost?
           </h1>
         </Link>
-      </TextOverImageFullscreen>
+      </TextOverImageFullscreen> */}
     </div>
   </>
 );
