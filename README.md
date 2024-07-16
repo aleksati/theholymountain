cef55017197ed4b2d8985fe5f4618ae0162f4e37
Aleksander Tidemann <aleksandertid@gmail.com>
2024-07-16 12:10:16 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # The Holy Mountain
 
-A website to promote my band. Page deployment status:
+A website and landing page to promote my band. A Nextjs website built with React and Tailwind CSS. Includes:
 
+- Likes counter with IP encryption and database integration
+- Custom and fully integrated web shop powered by STRIPE
+- Contact section with automatic mailing through node-mailer
+- Custom Audio and video players
+- Artist info supplied from database
+- Image gallery with modal pop-up images and key navigation
+- Google analytics
+- Theme selector
+- REST API
+
+and more..
+
+Page deployment status:
 [![Netlify Status](https://api.netlify.com/api/v1/badges/6601434d-210e-49d3-8613-ddc9df158900/deploy-status)](https://app.netlify.com/sites/theholymountain/deploys)
 
-# Run
+# How to Run
 
 For dev:
 
--- pages/api/musicdata.js
+++ pages/api/musicdata.js
@@ -2,84 +2,22 @@ import nextConnect from "next-connect";
 import connectMongo from "../../functions/connectMongo.js";
 import MusicData from "../../models/MusicData.js";
 import { commonApiHandlers } from "../../functions/commonApiHandlers.js";
-import {
-  initValidation,
-  check,
-  post,
-  del,
-  put,
-  oneOf,
-} from "../../middleware/middlewareApi.js";
+import { initValidation, check, post, del, put, oneOf } from "../../middleware/middlewareApi.js";
 
 const musicPostValidator = initValidation([
-  check("key")
-    .exists()
-    .withMessage("Key is missing")
-    .trim()
-    .notEmpty()
-    .withMessage("Key is empty")
-    .isLength({ min: 3 })
-    .withMessage("Key must be over 3 characters"),
-  check("title")
-    .exists()
-    .withMessage("Title is missing")
-    .trim()
-    .notEmpty()
-    .withMessage("Title is empty")
-    .isLength({ min: 3 })
-    .withMessage("Title must be over 3 characters"),
-  check("category")
-    .exists()
-    .withMessage("Category is missing")
-    .trim()
-    .notEmpty()
-    .withMessage("category is empty")
-    .isLength({ min: 3 })
-    .withMessage("Category must be over 3 characters"),
-  check("year")
-    .exists()
-    .withMessage("Year is missing")
-    .notEmpty()
-    .withMessage("Year is empty")
-    .isLength({ min: 3 })
-    .withMessage("Year must be over 3 characters"),
-  check("description")
-    .exists()
-    .withMessage("Description is missing")
-    .notEmpty()
-    .withMessage("Description is empty")
-    .isLength({ min: 3 })
-    .withMessage("Description must be over 3 characters"),
-  check("likes")
-    .exists()
-    .withMessage("Likes is missing")
-    .isArray()
-    .withMessage("Formats must be array"),
-  check("credits")
-    .exists()
-    .withMessage("Credits is missing")
-    .isArray({ min: 1 })
-    .withMessage("Credits must be array"),
+  check("key").exists().withMessage("Key is missing").trim().notEmpty().withMessage("Key is empty").isLength({ min: 3 }).withMessage("Key must be over 3 characters"),
+  check("title").exists().withMessage("Title is missing").trim().notEmpty().withMessage("Title is empty").isLength({ min: 3 }).withMessage("Title must be over 3 characters"),
+  check("category").exists().withMessage("Category is missing").trim().notEmpty().withMessage("category is empty").isLength({ min: 3 }).withMessage("Category must be over 3 characters"),
+  check("year").exists().withMessage("Year is missing").notEmpty().withMessage("Year is empty").isLength({ min: 3 }).withMessage("Year must be over 3 characters"),
+  check("description").exists().withMessage("Description is missing").notEmpty().withMessage("Description is empty").isLength({ min: 3 }).withMessage("Description must be over 3 characters"),
+  check("likes").exists().withMessage("Likes is missing").isArray().withMessage("Formats must be array"),
+  check("credits").exists().withMessage("Credits is missing").isArray({ min: 1 }).withMessage("Credits must be array"),
 ]);
 
-const musicDeleteValidator = initValidation([
-  check("key")
-    .exists()
-    .withMessage("Key is missing")
-    .trim()
-    .notEmpty()
-    .withMessage("key is empty")
-    .isLength({ min: 3 })
-    .withMessage("Key must be over 3 characters"),
-]);
+const musicDeleteValidator = initValidation([check("key").exists().withMessage("Key is missing").trim().notEmpty().withMessage("key is empty").isLength({ min: 3 }).withMessage("Key must be over 3 characters")]);
 
 const musicPutValidator = initValidation([
-  check("key")
-    .exists()
-    .withMessage("Key is missing")
-    .trim()
-    .notEmpty()
-    .withMessage("key is empty"),
+  check("key").exists().withMessage("Key is missing").trim().notEmpty().withMessage("key is empty"),
   oneOf([
     check("title").exists().withMessage("Title is required"),
     check("category").exists().withMessage("Category is missing"),
@@ -123,10 +61,7 @@ export default nextConnect()
     await connectMongo();
     const { deletedCount } = await MusicData.deleteOne({ key: req.body.key });
 
-    if (deletedCount === 0)
-      throw new Error(
-        `MusicData ${req.body.key} was not found and cannot be deleted.`
-      );
+    if (deletedCount === 0) throw new Error(`MusicData ${req.body.key} was not found and cannot be deleted.`);
 
     res.status(200).json({ message: `MusicData ${req.body.key} was removed` });
   })
